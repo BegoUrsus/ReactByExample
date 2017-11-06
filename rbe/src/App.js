cdb37d61a30adb5cbe6237fefa814b92e00f9d33
Begoña Huidobro Garriga <BegoUrsus@users.noreply.github.com>
2017-11-06 12:31:44 +0100

    to ch4

--- rbe/src/App.js
+++ rbe/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 import PropTypes from 'prop-types';
-
+import $ from 'jquery';
+import jquery from 'jquery';
+import timeago from 'timeago';
 
 class Heading extends Component {
   render() {
@@ -22,13 +25,13 @@ class Headings extends Component {
     var headings = this.props.headings.map(
       function(name, index) {
         return(
-          <Heading heading = {name} key={index}/>
+          <Heading heading = {name} key={"heading-"+index}/>
         );
       }
     );
     return (
       <thead>
-        <tr>
+        <tr className='table-th'>
           {headings}
         </tr>
       </thead>
@@ -39,8 +42,9 @@ class Headings extends Component {
 
 class Row extends Component {
   render() {
+    var trStyle = {backgroundColor: 'aliceblue'};
     return (
-      <tr key={this.props.changeSet.key}>
+      <tr key={this.props.changeSet.key} style={trStyle}>
         <td> { this.props.changeSet.when } </td>
         <td> { this.props.changeSet.who } </td>
         <td> { this.props.changeSet.description } </td>
@@ -75,6 +79,9 @@ class RecentChangesTable extends Component {
 
 
 
+
+
+
 class App extends Component {
 
   constructor(props) {
@@ -86,14 +93,96 @@ class App extends Component {
     }
   }
 
-  handleEvent (data) {
-    console.log("handleEvent");
-    this.setState({ changeSets: data.changeSets});
+  requestAuthor(key) {
+    var author = key;
+    return author;
+/*
+    $.ajax({
+      url: 'http://openlibrary.org' + key + '.json',
+      context: this,
+      dataType: 'json',
+      type: 'GET'
+    }).done(function (data) {
+      author = data.displayname;
+      console.log('done requestAuthor ' + author);
+      return (author);
+    });
+*/
   }
+
+
+
+  mapOpenLibraryDataToChangeSet(data) {
+    var mythis = this;
+    var datos = data.map(function (change, index) {
+      var mychange = {
+        "when": jquery.timeago(change.timestamp),
+        "who": mythis.requestAuthor(change.author.key),
+        "description": change.comment
+      }
+      return mychange;
+    });
+    return datos;
+    
+  }
+
+
+  fetchData() {
+    let changeSets = [];
+    $.ajax({
+      url: 'http://openlibrary.org/recentchanges.json?limit=10',
+      context: this,
+      dataType: 'json',
+      type: 'GET'
+    }).done(function (data) {
+      console.log("antes");
+      changeSets = this.mapOpenLibraryDataToChangeSet(data);
+      console.log("despues");
+      console.log(changeSets)
+      this.setState({changeSets: changeSets});
+    });
+  }
+
+  nestedAjax() {
+
+    $.get('http://openlibrary.org/recentchanges.json?limit=10').then(function(events) {
+      console.log(events);
+      var promises = $.map(events, function(change) {//$.map() loops through events.items and returns an array
+        console.log(change);
+          return $.get('http://openlibrary.org'+change.author.key+'.json').then(function(authordata) {
+            console.log(authordata);
+            change.authorname = authordata.displayname;
+            return change;
+          });
+      });
+
+      
+      $.when.apply(null, promises).then(function() {
+        let changeSets = [];
+        events.map(function(event) {
+          changeSets.push(
+            {
+              "when": jquery.timeago(event.timestamp),
+              "who": event.authorname,
+              "description": event.comment
+            }
+          );
+        })
+        console.log(changeSets);
+        return changeSets;
+      });
+
+  });
+};
+
+
 
 
   componentDidMount() {
     console.log("componentDidMount");
+    this.fetchData();
+    this.nestedAjax();
+    console.log("end componentDidMount");
   }
 
   componentWillMount() {
@@ -117,7 +206,7 @@ class App extends Component {
           </h1>
           <RecentChangesTable>
             <Headings headings={this.props.headings} />
-            <Rows changeSets={this.props.changeSets} />
+            <Rows changeSets={this.state.changeSets} />
           </RecentChangesTable>
         </section>
       
@@ -126,6 +215,8 @@ class App extends Component {
 
   }
 }
+
+
 
 App.conextTypes = {
   headings: PropTypes.array,
--- rbe/src/index.js
+++ rbe/src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -33,6 +34,46 @@ var props = {headings: headings, changeSets: data, title: title};
 ReactDOM.render(
     <App {...props} headings = {['Updated at', 'Author', 'Change']}/>, 
     document.getElementById('root')
+);
+
+
+class InputExample extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value.toUpperCase() });
+  }
+  
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+  
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>    
+    );
+  }
+}
+
+
+ReactDOM.render(
+  <InputExample />,
+    document.getElementById('myform')
 );
 
 registerServiceWorker();
